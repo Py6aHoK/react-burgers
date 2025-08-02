b1269062ec785121af21a26f2f2277718df6d660
Py6aHoK <ilya.shuvalov.35@gmail.com>
2025-08-02 17:03:34 +0300

    исправление замечаний

--- src/components/order-page/order-page.tsx
+++ src/components/order-page/order-page.tsx
@@ -1,16 +1,32 @@
 import { getOrder } from '@/services/actions/order';
 import { useAppDispatch, useAppSelector } from '@/utils/hooks';
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { FeedOrderDetails } from '../feed-order-details/feed-order-details';
+import { useCallback, useEffect, useState } from 'react';
+import {
+	NavigateFunction,
+	useLocation,
+	useNavigate,
+	useParams,
+} from 'react-router-dom';
 import { Preloader } from '../preloader/preloader';
+import { Modal } from '../modal/modal';
+import { Status } from '../status/status';
+import { IngredientsList } from '../ingredients-list/ingredients-list';
+import { FormattedDate } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Price } from '../price/price';
+import { TIngredient } from '@/utils/types';
+import { useOrderInfo } from '@/utils/order';
+import styles from './order-page.module.css';
 
 export const OrderPage = (): React.JSX.Element => {
-	const { orderInfo, getOrderRequest, getOrderRequestError } = useAppSelector(
-		(state) => state.order
-	);
+	const navigate: NavigateFunction = useNavigate();
+	const { orderInfo, getOrderRequest } = useAppSelector((state) => state.order);
 	const dispatch = useAppDispatch();
 	const { id } = useParams<string>();
+	const { getIngredients, getPrice } = useOrderInfo();
+	const [currentIngredients, setIngredients] = useState<TIngredient[]>([]);
+	const [price, setPrice] = useState<number>(0);
+	const location = useLocation();
+	const [number, setNumber] = useState<string | undefined>();
 
 	useEffect((): void => {
 		if (id) {
@@ -18,12 +34,47 @@ export const OrderPage = (): React.JSX.Element => {
 		}
 	}, [dispatch, id]);
 
-	if (getOrderRequest || !orderInfo) {
-		return <Preloader />;
-	}
-	if (getOrderRequestError) {
-		return <>Ошибка получения данных</>;
-	}
+	const handleCloseModal = useCallback(() => {
+		void navigate(location.pathname.split('/').slice(0, -1).join('/'));
+	}, [navigate]);
 
-	return <FeedOrderDetails />;
+	useEffect((): void => {
+		if (!orderInfo) {
+			return;
+		}
+		const orderIngredients: TIngredient[] = getIngredients(
+			orderInfo.ingredients
+		);
+		setIngredients(orderIngredients);
+		setPrice(getPrice(orderIngredients));
+		setNumber('#' + orderInfo.number.toString().padStart(6, '0'));
+	}, [dispatch, orderInfo]);
+
+	return (
+		<>
+			<Modal title={number} closeHandler={handleCloseModal}>
+				{getOrderRequest ? (
+					<Preloader />
+				) : orderInfo ? (
+					<div className={`${styles.order} mt-10`}>
+						<div className='text_type_main-medium mb-3'>{orderInfo.name}</div>
+						<Status className='mb-15' status={orderInfo.status} />
+						<div className='text_type_main-medium mb-6'>Состав:</div>
+						<IngredientsList ingredients={currentIngredients} />
+						<div className={styles.info}>
+							{orderInfo && (
+								<FormattedDate
+									date={new Date(orderInfo.createdAt)}
+									className='text_type_main-default text_color_inactive mt-10'
+								/>
+							)}
+							<Price>{price}</Price>
+						</div>
+					</div>
+				) : (
+					<>Ошибка получения данных</>
+				)}
+			</Modal>
+		</>
+	);
 };
