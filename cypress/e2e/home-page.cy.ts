261790062d6bd8e89c4bb51afd42bb814ef38f95
Py6aHoK <ilya.shuvalov.35@gmail.com>
2025-08-02 17:29:21 +0300

    Update home-page.cy.ts

--- cypress/e2e/home-page.cy.ts
+++ cypress/e2e/home-page.cy.ts
@@ -3,17 +3,16 @@ const testDetails: string = 'Детали ингредиента';
 const testProfile: string = 'Личный кабинет';
 const testConstructor: string = '#burgerConstructor';
 
-describe('service is available', function () {
+describe('Сервис доступен', function () {
 	before(function () {
 		cy.visit('/');
 	});
 
-	it('should open constructor page by default', function () {
+	it('Проверка открытия страницы конструктора', function () {
 		cy.contains('Соберите бургер');
 	});
 
-	it('should log in', function () {
-		cy.visit('/');
+	it('Проверка авторизации', function () {
 		cy.contains(testProfile).click();
 		cy.get('.input').as('login-form');
 		cy.get('@login-form').find('[class^=text]').first().as('email-input');
@@ -27,40 +26,52 @@ describe('service is available', function () {
 		cy.contains('Войти').click();
 	});
 
-	it('should open and close modal by icon', function () {
-		cy.contains(testIngredient).click();
-		cy.contains(testDetails).should('be.visible');
-		cy.contains(testIngredient).should('be.visible');
-		cy.get('#modalCloseButton').click();
-	});
+	it(
+		'Проверка открытие модального окна с описанием ингредиента, ' +
+			'отображения данных о нем и закрытия при нажатии на кнопку закрытия',
+		function () {
+			cy.contains(testIngredient).click();
+			cy.contains(testDetails).should('be.visible');
+			cy.contains(testIngredient).should('be.visible');
+			cy.get('#modalCloseButton').click();
+		}
+	);
 
-	it('should open and close modal by overlay', function () {
-		cy.contains(testIngredient).click();
-		cy.contains(testDetails).should('be.visible');
-		cy.contains(testIngredient).should('be.visible');
-		cy.get('#modalOverlay').click(-20, -20, { force: true });
-	});
+	it(
+		'Проверка открытие модального окна с описанием ингредиента, ' +
+			'отображения данных о нем и закрытия при нажатии на подложку',
+		function () {
+			cy.contains(testIngredient).click();
+			cy.contains(testDetails).should('be.visible');
+			cy.contains(testIngredient).should('be.visible');
+			cy.get('#modalOverlay').click(-20, -20, { force: true });
+		}
+	);
 
-	it('should check drag and drop and make order', function () {
-		cy.contains(testIngredient).trigger('dragstart');
-		cy.get(testConstructor).trigger('drop');
-		cy.get(testConstructor).contains(testIngredient);
-		cy.contains('Биокотлета из марсианской Магнолии').trigger('dragstart');
-		cy.get(testConstructor).trigger('drop');
-		cy.get(testConstructor).contains('Биокотлета из марсианской Магнолии');
-		cy.contains('Кристаллы марсианских альфа-сахаридов').trigger('dragstart');
-		cy.get(testConstructor).trigger('drop');
-		cy.get(testConstructor).contains('Кристаллы марсианских альфа-сахаридов');
-		cy.contains('Соус Spicy-X').trigger('dragstart');
-		cy.get(testConstructor).trigger('drop');
-		cy.get(testConstructor).contains('Соус Spicy-X');
-		cy.contains('Оформить заказ').click();
-		cy.wait(20000);
-		cy.contains('Ваш заказ начали готовить').should('exist');
-		cy.get('#modalCloseButton').click();
-	});
+	it(
+		'Проверка перетаскивания ингредиента в конструктор и ' +
+			'открытия модалки после оформления заказа',
+		function () {
+			cy.contains(testIngredient).trigger('dragstart');
+			cy.get(testConstructor).trigger('drop');
+			cy.get(testConstructor).contains(testIngredient);
+			cy.contains('Биокотлета из марсианской Магнолии').trigger('dragstart');
+			cy.get(testConstructor).trigger('drop');
+			cy.get(testConstructor).contains('Биокотлета из марсианской Магнолии');
+			cy.contains('Кристаллы марсианских альфа-сахаридов').trigger('dragstart');
+			cy.get(testConstructor).trigger('drop');
+			cy.get(testConstructor).contains('Кристаллы марсианских альфа-сахаридов');
+			cy.contains('Соус Spicy-X').trigger('dragstart');
+			cy.get(testConstructor).trigger('drop');
+			cy.get(testConstructor).contains('Соус Spicy-X');
+			cy.contains('Оформить заказ').click();
+			cy.wait(20000);
+			cy.contains('Ваш заказ начали готовить').should('exist');
+			cy.get('#modalCloseButton').click();
+		}
+	);
 
-	it('should log out', function () {
+	it('Проверка разавторизации', function () {
 		cy.contains(testProfile).click();
 		cy.contains('Выход').click();
 		cy.contains('Конструктор').click();
